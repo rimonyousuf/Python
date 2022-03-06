4ef21289e48213a0c0ae4ab1774efd2a3abfb551
rimon-yousuf <rimonyousuf00@gmail.com>
2022-03-06 21:53:41 +0600

    added

---
+++ Basic/4.Type Casting.py
@@ -0,0 +1,19 @@
+num1=float(input("Enter first number = "))
+num2=float(input("Enter second number = "))
+
+print("\n")
+
+sum=num1+num2
+print("Addition is =",sum)
+
+sub=num1-num2
+print("Substraction is =",sub)
+
+mul=num1*num2
+print("Multiplication is =",mul)
+
+div=num1/num2
+print("Division is =",div)
+
+rem=num1%num2
+print("Remainder is =",rem)
