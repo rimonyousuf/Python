b7781cf48bc03ab765956a75746130a85c39ed31
rimon-yousuf <rimonyousuf00@gmail.com>
2022-03-07 17:40:36 +0600

    added

---
+++ Basic/10.if,elif,else statement.py
@@ -0,0 +1,22 @@
+mark=float(input("Enter your mark : "))
+
+if mark>=80:
+    print("A+")
+
+elif mark>=70:
+    print("A")
+
+elif mark>=60:
+    print("A-")
+
+elif mark>=50:
+    print("B")
+
+elif mark>=40:
+    print("C")
+
+elif mark>=30:
+    print("D")
+
+else:
+    print("You have failed")
