0aa2b24421141ebaebb52a399889f068077f8739
rimon-yousuf <rimonyousuf00@gmail.com>
2022-03-08 17:13:22 +0600

    added

---
+++ Basic/16.Sum of n numbers.py
@@ -0,0 +1,10 @@
+m=int(input("Enter the starting number = "))
+n=int(input("Enter the ending number = "))
+
+sum=0
+i=m
+while i<=n :
+    sum=sum+i
+    i=i+1
+
+print("Sum all of these numbers = ",sum)
