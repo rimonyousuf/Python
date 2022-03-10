b4e7defa2873447e3140c76223484bba4806bcd4
rimon-yousuf <rimonyousuf00@gmail.com>
2022-03-10 17:52:50 +0600

    added

---
+++ Basic/23.Pattern printing.py
@@ -0,0 +1,19 @@
+"""
+*
+**
+***
+"""
+
+n=int(input("Enter ammount : "))
+for x in range(n+1):
+    print(x*"*")
+
+"""
+*
+***
+*****
+"""
+
+m=int(input("Enter ammount : "))
+for y in range(m+1):
+    print((2*y-1)*"*")
