915b7bf058017fc284702e1ead8bbdc6acad9c19
rimon-yousuf <rimonyousuf00@gmail.com>
2022-03-10 17:28:18 +0600

    added

---
+++ Basic/21.for Loop.py
@@ -0,0 +1,15 @@
+num=[11,22,33,44,55]
+print(num)
+
+sum=0
+for x in num :
+    sum=sum+x
+print(sum)
+
+"""
+    index=0
+    i=len(num)
+    while index < i :
+        print(num[index])
+        index=index+1
+"""
