74625f33fb61e000728a6639ed5dacacbf3d2522
rimon-yousuf <rimonyousuf00@gmail.com>
2022-03-10 16:36:13 +0600

    added

---
+++ Basic/18.List part 1.py
@@ -0,0 +1,10 @@
+subjects=["C","C++","Java","Python","Android","OS"]
+
+print(subjects)
+print(subjects[2])
+print(subjects[2:])
+print(subjects[-2])
+print("HTML" in subjects)
+print("CSS" not in subjects)
+print("Java" in subjects)
+print(subjects + ["ML"])
