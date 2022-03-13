7f07815c026ff09bc3e558d40dc4fd1b4056e7bd
rimon-yousuf <rimonyousuf00@gmail.com>
2022-03-13 16:56:20 +0600

    added

---
+++ 9.Introduction to Function.py
@@ -0,0 +1,14 @@
+def add(x,y):
+    sum=x+y
+    print("Sum is =",sum)
+
+def addition(x,y,z):
+    sums=x+y+z
+    print("Sum of 3 digits is =",sums)
+
+def message():
+    print("No parameter")
+
+add(20,10)
+addition(10.5,30,25.69)
+message()
