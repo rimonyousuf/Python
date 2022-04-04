e67727321d68fc991dc567ad62391e59c59b9e33
rimon-yousuf <rimonyousuf00@gmail.com>
2022-04-05 05:56:43 +0600

    Class and Object

---
+++ OOP/1.class and object.py
@@ -0,0 +1,18 @@
+class Student:
+    roll=""
+    gpa=""
+
+karim=Student()
+karim.roll=101
+karim.gpa=4.73
+print(f"Roll : {karim.roll}, GPA : {karim.gpa}")
+
+arafat=Student()
+arafat.roll=102
+arafat.gpa=4.23
+print(f"Roll : {arafat.roll}, GPA : {arafat.gpa}")
+
+monir=Student()
+monir.roll=103
+monir.gpa=2.78
+print(f"Roll : {monir.roll}, GPA : {monir.gpa}")
